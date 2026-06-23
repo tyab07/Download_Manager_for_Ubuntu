34f0719c354f86d501939819dcb351b97ece9784
tyab07 <tyab.mujahid@gmail.com>
2026-06-23 22:49:48 +0500

    feat(core): initialize VideoExtractor class with download_dir and callbacks

--- core/video_extractor.py
+++ core/video_extractor.py
@@ -7,3 +7,19 @@ import os
 import json
 from typing import Optional, Callable
 from pathlib import Path
+
+
+class VideoExtractor:
+    """Extracts video info and downloads using yt-dlp."""
+
+    def __init__(self, download_dir: str = None):
+        self.download_dir = download_dir or str(Path.home() / "Downloads")
+        self._progress_callback: Optional[Callable] = None
+        self._status_callback: Optional[Callable] = None
+
+    def set_progress_callback(self, callback: Callable):
+        self._progress_callback = callback
+
+    def set_status_callback(self, callback: Callable):
+        self._status_callback = callback
+
